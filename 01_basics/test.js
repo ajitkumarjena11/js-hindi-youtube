60d316c932e18f65f85c93f97b83595cc6646d99
ajitkumarjena11 <158299220+ajitkumarjena11@users.noreply.github.com>
2024-05-01 19:16:46 +0000

    elven commit

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -2,6 +2,7 @@ const accountId = 12564
 let accountEmail = "ajit@gmail.com"
 var accountPassword = "12345"
 accountCity = "Bangalore"
+let accountState;
 
 //acccountId = 2// not allowed
 
@@ -10,4 +11,9 @@ accountEmail = "aj@gmail.com"
 accountPassword = "1237645"
 accountCity = "Odishahudhudgythodeuidejuuyu675u"
 
-console.table([accountId, accountEmail, accountPassword, accountCity])
+/*
+Prefer npot to use var 
+because of issue in block scope and functional scope 
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
