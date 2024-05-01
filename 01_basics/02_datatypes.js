9f92350396ae11f66f3dcc2aba3efcba0945c515
ajitkumarjena11 <158299220+ajitkumarjena11@users.noreply.github.com>
2024-05-01 20:02:52 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict";// total all JS code as newer version
+
+//alert("hello") //we are using nodejs, not brower
+
+console.log(3
+    + 3)// code readability should be high
+
+console.log("ajit")
+
+let name = "ajit"
+let age = 18;
+let isLoggedId = false
+let state = null
+
+//number => 2 to power 53
+//bigint
+//string =>""
+//boolean =>true/false
+//null =>stamdlone value
+//undefine =>
+//symbol =>uniuqe
+
+
+//object
+console.log(typeof undefined); //undefine
+console.log(typeof null); //object
