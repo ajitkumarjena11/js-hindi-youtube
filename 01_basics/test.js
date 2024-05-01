f7da1d13348d64e1a609fd17da9ca3972cd4928e
ajitkumarjena11 <158299220+ajitkumarjena11@users.noreply.github.com>
2024-05-01 18:29:08 +0000

    four commit

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -8,6 +8,6 @@ accountCity = "Bangalore"
 
 accountEmail = "aj@gmail.com"
 accountPassword = "1237645"
-accountCity = "Odishaaaa"
+accountCity = "Odishahudhude"
 
 console.table([accountId, accountEmail, accountPassword, accountCity])
