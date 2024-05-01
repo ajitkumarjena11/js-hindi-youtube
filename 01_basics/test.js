01e223514de862e1639c48666397ef3ab1092010
ajitkumarjena11 <158299220+ajitkumarjena11@users.noreply.github.com>
2024-05-01 18:18:13 +0000

    second commit

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,13 @@
-console.log("Ajit Kumar Jena");
+const accountId = 12564
+let accountEmail ="ajit@gmail.com"
+var accountPassword ="12345"
+accountCity ="Bangalore"
+
+//acccountId = 2// not allowed
+
+
+accountEmail ="aj@gmail.com"
+accountPassword ="1237645"
+accountCity ="Odisha"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
